502a8e61344849ef4b27114869ee57ead6c6fba7
hedi-guedidi <hedi.guedidi@dauphine.eu>
2020-03-22 13:47:53 +0100

    second commit

--- PrintExec.java
+++ PrintExec.java
@@ -6,8 +6,8 @@ public class PrintExec {
 			if(nameclass.charAt(0)=='\''){
 				nameclass.substring(1,nameclass.length()-1);
 			}
-			str1=str1+args[0]+nameclass;
-			str2=str2+args[1];
+			str1=str1+"-cp "+"'.:"+args[0]+"' "+"'"+args[0]+args[1]+".java'"+((args[args.length-1].equals("true"))?" -d bin/":"");
+			str2=str2+"-cp "+"'.:"+args[0]+((args[args.length-1].equals("true"))?":bin/":"")+"' "+args[1];
 			System.out.println(str1);
 			System.out.println(str2);
 }
